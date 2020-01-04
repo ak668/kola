e8f03560735c2d96b8754d6a8a9409f782d4c13f
ak668 <ak668@protonmail.com>
2020-01-04 13:14:23 +0000

    Update kbot.js

--- kbot.js
+++ kbot.js
@@ -6,4 +6,4 @@ new Client({
     cmdEditing: true,
     typing: true,
     readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`
-}).login('MzI2NjIwMzcxMDgyMjgwOTYx.DCpcfQ.yzT4fWqJYx9dGKEORHUpLxJvpP0');
+}).login('');
